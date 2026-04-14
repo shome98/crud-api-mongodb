ae3d6ee7caeeb0a2006defb0a279d9f46d9daa77
Sowparna Shome <61792308+shome98@users.noreply.github.com>
2026-04-14 18:24:42 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB().then(() =>
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT} 🔥`);
+        console.log(`Server is running on http://localhost:${PORT} 🔥`);
     })).catch((error) => console.log(`Error: ${error.message} ❌`));
 
 
